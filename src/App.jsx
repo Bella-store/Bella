010c39a8ee46f2d0775b21c01e70e435b152520b
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2024-08-26 23:38:57 +0300

    add home page to route to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Home from "./pages/Home";
 
 function App() {
     return (
         <>
-            <div></div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+            </Routes>
         </>
     );
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <ul className="flex flex-col space-y-4">
+                <li>
+                    <a href="/cart">Cart Page</a>
+                </li>
+                <li>
+                    <a href="/checkout">Checkout Page</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
