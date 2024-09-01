8a848648e108a7d6510696b6de1e8cd10b968776
mahmoudAkram <mahmoudakrm20@gmail.com>
2024-09-01 23:14:47 +0300

    Add Contact us / some adjustments for other pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,29 @@
 export default function Home() {
-    return (
-        <div>
-            <ul className="flex flex-col space-y-4">
-                <li>
-                    <a href="/login">Login Page</a>
-                </li>
-                <li>
-                    <a href="/register">Register Page</a>
-                </li>
-                <li>
-                    <a href="/products">Products Page</a>
-                </li>
-                <li>
-                    <a href="/cart">Cart Page</a>
-                </li>
-                <li>
-                    <a href="/checkout">Checkout Page</a>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul className="flex flex-col space-y-4">
+        <li>
+          <a href="/login">Login Page</a>
+        </li>
+        <li>
+          <a href="/register">Register Page</a>
+        </li>
+        <li>
+          <a href="/products">Products Page</a>
+        </li>
+        <li>
+          <a href="/cart">Cart Page</a>
+        </li>
+        <li>
+          <a href="/checkout">Checkout Page</a>
+        </li>
+        <li>
+          <a href="/contactus">Contact Us</a>
+        </li>
+        <li>
+          <a href="/wishlist">Wish List</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
