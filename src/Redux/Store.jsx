78d18605c424804a961b3ad0d2600a91fc1c426f
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2024-09-07 15:30:30 +0300

    complete merging

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./Slices/ProductsSlice";
 import authReducer from "./Slices/AuthSlice";
 import cartReducer from "./Slices/CartSlice";
+import favouriteReducer from "./Slices/FavouriteSlice";
 
 const storeApp = configureStore({
     reducer: {
         products: productsReducer,
         auth: authReducer,
         cart: cartReducer,
+        favourites: favouriteReducer,
     },
 });
 export default storeApp;
